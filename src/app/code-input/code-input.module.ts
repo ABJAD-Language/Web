5471fab47afce074ef675b36c94e78057b9aab40
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2022-11-26 16:28:20 +0200

    add full screen mode for code viewing

--- src/app/code-input/code-input.module.ts
+++ src/app/code-input/code-input.module.ts
@@ -5,10 +5,11 @@ import {NzInputModule} from "ng-zorro-antd/input";
 import {FormsModule} from "@angular/forms";
 import {NzButtonModule} from "ng-zorro-antd/button";
 import {NzIconModule} from "ng-zorro-antd/icon";
+import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @NgModule({
   declarations: [CodeInputComponent],
-  imports: [CommonModule, NzInputModule, FormsModule, NzButtonModule, NzIconModule],
+  imports: [CommonModule, NzInputModule, FormsModule, NzButtonModule, NzIconModule, NzModalModule],
   exports: [
     CodeInputComponent
   ]
